91c4ac2c478adde2941e549820d71f5f34bfbe33
Aaron Romel <aromel@forbes.com>
2018-07-16 16:38:59 -0400

    lint fixes for webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,49 @@
-var path = require('path');
+const path = require('path');
 
 module.exports = [{
-        mode: 'production',
-        entry: './src/index.js',
-        output: {
-            path: path.resolve(__dirname, 'dist'),
-            filename: 'bundle.js'
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: './',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/, /dist/],
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['babel-preset-env'],
+          },
         },
-        devServer:{
-            contentBase: './'
+      },
+    ],
+  },
+},
+{
+  mode: 'development',
+  entry: './demo/demo.js',
+  output: {
+    path: path.resolve(__dirname, 'demo'),
+    filename: 'demo.bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/, /dist/],
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['babel-preset-env'],
+          },
         },
-        module: {
-            rules: [
-            {
-                test: /\.js$/,
-                exclude: [/node_modules/, /dist/],
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['babel-preset-env']
-                    }
-                }
-            }
-            ]
-        },
-    },
-    {
-        mode: 'development',
-        entry: './demo/demo.js',
-        output: {
-            path: path.resolve(__dirname, 'demo'),
-            filename: 'demo.bundle.js'
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.js$/,
-                    exclude: [/node_modules/, /dist/],
-                    use: {
-                        loader: 'babel-loader',
-                        options: {
-                            presets: ['babel-preset-env']
-                        }
-                    }
-                }
-            ]
-        }
+      },
+    ],
+  },
 }];
